69e7dee1a28d877f7f23e199838ef3786a6be7df
aditisharma <asharma17932@gmail.com>
2024-11-21 13:59:58 -0800

    first commit

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Rooms.css';
 
 const Rooms = () => {
-  const imagePath = "https://images.woodenstreet.de/image/data%2FLooks%2F9.jpg";
+  const imagePath = "https://mir-s3-cdn-cf.behance.net/project_modules/1400/1defc144143987.5808bd1b9eb91.jpg";
   const handleClick = () => {
     alert("Booking functionality coming soon!");
   };
@@ -10,27 +10,36 @@ const Rooms = () => {
   return (
     <section className="rooms" id="rooms">
       <h2>Our Rooms</h2>
-      <div className="room-list">
+      <div className="room-list" style={{}}>
         <div className="room">
           <img src={imagePath} alt="Luxury Suite" />
-          <h3>Luxury Suite</h3>
+          <h3 >Luxury Suite</h3>
           <p>Spacious, elegant, with a stunning view.</p>
-          <h5>Starting from <span>$399/night</span></h5>
-          <button onClick={handleClick}>Book Now</button>
+          <div className="room-info">
+            <h5>Starting from <span>$399/night</span></h5>
+            <button onClick={handleClick}>Book Now</button>
+          </div>
         </div>
+
         <div className="room">
           <img src="./ac.jpeg" width="300"  alt="Deluxe Room" />
-          <h3>Deluxe Room</h3>
+          <h3 >Deluxe Room</h3>
           <p>Comfort and style with modern amenities.</p>
-          <h5>Starting from <span>$399/night</span></h5>
-          <button onClick={handleClick}>Book Now</button>
+          <div className="room-info">
+            <h5>Starting from <span>$399/night</span></h5>
+            <button onClick={handleClick}>Book Now</button>
+          </div>
+          
         </div>
         <div className="room">
           <img src="./vip.jpeg" width="40" alt="Standard Room" />
-          <h3>Standard Room</h3>
+          <h3 >Standard Room</h3>
           <p>Cozy and affordable, perfect for a short stay.</p>
-          <h5>Starting from <span>$399/night</span></h5>
-          <button onClick={handleClick}>Book Now</button>
+          <div className="room-info">
+            <h5>Starting from <span>$399/night</span></h5>
+            <button onClick={handleClick}>Book Now</button>
+          </div>
+          
         </div>
       </div>
     </section>
